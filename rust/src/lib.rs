0904055b0d4ddf8bde83298f0da3adf0e23f1390
Alex Samuel <alex@alexsamuel.net>
2016-04-29 01:01:21 -0400

    Some Rust date stuff.

---
+++ rust/src/lib.rs
@@ -0,0 +1,115 @@
+pub type Year = u16;
+pub type Month = u8;
+pub type Day = u8;
+
+pub fn is_leap_year(year: Year) -> bool {
+    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
+}
+
+pub fn days_in_month(year: Year, month: Month) -> Day {
+    match month {
+        1 | 3 | 5 | 7 | 8 | 10 | 12 => 31,
+        4 | 6 | 9 | 11 => 30,
+        2 => if is_leap_year(year) { 29 } else { 28 }, 
+        _ => panic!("invalid month: {}", month),
+    }
+}
+
+//------------------------------------------------------------------------------
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct Date {
+    year: Year,
+    month: Month,
+    day: Day,
+}
+    
+fn between<T: std::cmp::PartialOrd>(min: T, value: T, max: T) -> bool {
+    min <= value && value <= max
+}
+
+impl Date {
+    pub fn new(year: Year, month: Month, day: Day) -> Date {
+        let date = Date{ year: year, month: month, day: day };
+        assert!(date.valid());
+        date
+    }
+
+    pub fn valid(&self) -> bool {
+        between(1, self.year, 9999)
+            && between(1, self.month, 12)
+            && between(1, self.day, days_in_month(self.year, self.month))
+    }
+
+    pub fn next(&self) -> Date {
+        let mut date = *self;
+        date.day += 1;
+        if date.day > days_in_month(date.year, date.month) {
+            date.day = 1;
+            date.month += 1;
+            if date.month > 12 {
+                date.month = 1;
+                date.year += 1;
+            }
+        }
+        date
+    }
+
+    pub fn prev(&self) -> Date {
+        let mut date = *self;
+        date.day -= 1;
+        if date.day < 1 {
+            date.month -= 1;
+            if date.month < 1 {
+                date.year -= 1;
+                date.month = 12;
+            }
+            date.day = days_in_month(date.year, date.month);
+        }
+        date
+    }
+
+    pub fn shift(self, count: i32) -> Date {
+        if count > 0 {
+            (0 .. count).fold(self, |d, _| d.next())
+        }
+        else if count < 0 {
+            (0 .. -count).fold(self, |d, _| d.prev())
+        }
+        else {
+            self
+        }
+    }
+
+}
+
+//------------------------------------------------------------------------------
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_leap_year_test() {
+        assert!(!is_leap_year(1999));
+        assert!( is_leap_year(2000));
+        assert!(!is_leap_year(2001));
+        assert!(!is_leap_year(2002));
+        assert!( is_leap_year(2004));
+        assert!( is_leap_year(2040));
+        assert!(!is_leap_year(2100));
+    }
+
+    #[test]
+    fn shift_test() {
+        let first = Date::new(1, 1, 1);
+        assert_eq!(first.shift(  0), Date::new(1,  1,  1));
+        assert_eq!(first.shift(  1), Date::new(1,  1,  2));
+        assert_eq!(first.shift(  2), Date::new(1,  1,  3));
+        assert_eq!(first.shift( 30), Date::new(1,  1, 31));
+        assert_eq!(first.shift( 31), Date::new(1,  2,  1));
+        assert_eq!(first.shift(365), Date::new(2,  1,  1));
+        assert_eq!(Date::new(1, 12, 31).shift(1), Date::new(2, 1, 1));
+    }
+}
+
